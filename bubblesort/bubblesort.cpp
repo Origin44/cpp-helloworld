6975f44bd1a57c4d2de467ad302a713aff7b2248
Origin44 <1538713331@qq.com>
2021-02-10 00:13:57 +0800

    algorithm optimization

--- bubblesort/bubblesort.cpp
+++ bubblesort/bubblesort.cpp
@@ -6,17 +6,21 @@ int main()
 {
 	int a[]={16,8,4,32,5};
 	int i,j;
+	bool swap;
 	for (i=0;i<5;i++)
 	{
+		swap=false;
 		for (j=0;j<5-i-1;j++)
 		{
 			if (a[j]>a[j+1])
 			{
+				swap=true;
 				a[j]^=a[j+1];
 				a[j+1]^=a[j];
 				a[j]^=a[j+1];
 			}
 		}
+		if (!swap) break;
 	}
 	for (i=0;i<5;i++)
 	{
